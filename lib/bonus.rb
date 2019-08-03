bee6524918e2366fc6c4b102ada0c18a3ef7a588
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-08-03 04:35:43 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,7 +19,7 @@ def bonus
       ]
    }
   }
-
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 end
-epic_tragedy[:montague][:hero][:status] = "dead"
-epic_tragedy[:capulet][:heroine][:status] = "dead"
+
